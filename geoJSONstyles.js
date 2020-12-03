434dcd844febac44e195ef0633e05024b1cdf23b
hmfriese <59839846+hmfriese@users.noreply.github.com>
2020-12-02 23:57:08 -0700

    Add files via upload
    uploading js files for geoJSON+style variables

---
+++ geoJSONstyles.js
@@ -0,0 +1,13 @@
+var pre1975style = {
+	"fillColor": "000000",
+	"weight": 5,
+	"opacity": 0.65,
+	"color": "blue"
+};
+
+var post1975style = {
+	"fillColor": "#564E58",
+	weight: 5,
+	"opacity": 0.65,
+	"color": "#7D4F50"
+};
