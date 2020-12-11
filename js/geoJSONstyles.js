cb63a5bb499786943516109430dbcfcf40a91c18
hmfriese <59839846+hmfriese@users.noreply.github.com>
2020-12-11 13:03:55 -0700

    Update geoJSONstyles.js

--- js/geoJSONstyles.js
+++ js/geoJSONstyles.js
@@ -1,20 +1,27 @@
 var pre1975style = {
 	"fillColor": "000000",
-	"weight": 5,
+	"weight": 2,
 	"opacity": 0.65,
-	"color": "blue"
+	"color": "blue",
+	"dashArray": 3,
+    "fillOpacity":0.7
 };
 
 var post1975style = {
 	"fillColor": "#564E58",
-	weight: 5,
+	"weight": 2,
 	"opacity": 0.65,
-	"color": "#7D4F50"
+	"color": "#7D4F50",
+	"dashArray": 5,
+	"fillOpacity":0.7
 };
 
 var territoryStyle = {
 	"fillColor": "#303036",
-	weight: 5,
+	"weight": 5,
 	"opacity": 0.65,
-	"color": "#303036"
+	"color": "#303036",
+	"dashArray": 3,
+	"fillOpacity":0.7
 };
+
